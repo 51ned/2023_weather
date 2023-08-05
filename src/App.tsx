e28e4f23f4ba6313f988f71a3c8f6689f2e7da8c
Denis <sustain@bk.ru>
2023-08-05 19:26:40 +0300

    final cut .

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { MainLayout } from './views/layouts/main'
 
 import { mainStore } from './stores'
 
-import { Canvas, Placeholder, WeatherApp } from './views'
+import { Canvas, WeatherApp } from './views'
 
 
 export default function Home() {
-  const points = mainStore.points
+  const points = mainStore.points ?? []
   
   return (
     <MainLayout>
       <WeatherApp>
-        { !points ? <Placeholder /> : <Canvas points={points} /> }
+        <Canvas points={points} />
       </WeatherApp>
     </MainLayout>
   )
